f2f92184c00ad0fc4cd3b48ca7c0507d4cc7c81d
Chuyen Luu <chuyen.luu@ffwagency.com>
2017-12-08 18:38:09 +0700

    dnd beautiful, dnd cards and sort

--- src/index.js
+++ src/index.js
@@ -1,23 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Draggable, Droppable } from 'react-drag-and-drop'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import './index.css';
 
+// using some little inline style helpers to make the app look okay
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background color if dragging
+  background: isDragging ? 'lightgreen' : 'white',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : '',
+  padding: grid,
+  width: 250,
+});
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 class CardItem extends React.Component {
   render() {
+    const item = this.props.item;
+
     return (
-      <Draggable type="card" data={this.props.item.id}><li>{this.props.item.name}</li></Draggable>
+      <Draggable key={item.id} draggableId={item.id}>
+        {(provided, snapshot) => (
+          <div>
+            <div
+              ref={provided.innerRef}
+              style={getItemStyle(
+                provided.draggableStyle,
+                snapshot.isDragging
+              )}
+              {...provided.dragHandleProps}
+            >
+              {item.name}
+            </div>
+            {provided.placeholder}
+          </div>
+        )}
+      </Draggable>
     );
   }
 }
 
 class CardsList extends React.Component {
-  onDrop(data) {
-    this.props.onDnD(data.card, this.props.name);
-  }
-
   render() {
     let items = [];
     this.props.cards.forEach(card => {
@@ -27,14 +69,18 @@ class CardsList extends React.Component {
     });
 
     return (
-      <Droppable
-        types={['card']}
-        onDrop={this.onDrop.bind(this)}>
-        <div className="list-cards">
-          <h2>{this.props.name}</h2>
-          <ul className="cards dropable">{items}</ul>
-        </div>
-      </Droppable>
+      <div className="droppable">
+        <h4>{this.props.name}</h4>
+        <Droppable droppableId={this.props.name}>
+          {(provided, snapshot) => (
+            <div className="list" ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
+              {items}
+              {provided.placeholder}
+              <p>Drop card here</p>
+            </div>
+          )}
+        </Droppable>
+      </div>
     );
   }
 }
@@ -42,75 +88,73 @@ class CardsList extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleDnD = this.handleDnD.bind(this);
+    this.state = {
+      cards: this.props.cards,
+      groups: this.props.groups
+    }
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
 
-  handleDnD(id, group) {
-    let newCards = [];
-    this.props.cards.forEach((card) => {
-      if (card.id === id) {
-        card.group = group;
-      }
+  onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
 
-      newCards.push(card);
-    });
+    if (result.destination.droppableId === result.source.droppableId) {
+      let from = this.state.cards.filter(item => {return item.group === result.destination.droppableId;});
+      let fromOrder = reorder(from, result.source.index, result.destination.index);
+      let ortherTo = this.state.cards.filter(item => {return item.group !== result.destination.droppableId;});
 
-    this.setState({
-      cards: newCards
-    });
+      this.setState({cards: [...fromOrder, ...ortherTo]});
+    } else {
+      this.state.cards.forEach(item => {
+        if (item.id === result.draggableId) {
+          item.group = result.destination.droppableId;
+        }
+      });
+
+      let to = this.state.cards.filter(item => {return item.group === result.destination.droppableId;});
+      let index = to.findIndex(item => item.id === result.draggableId);
+      let toOrder = reorder(to, index, result.destination.index)
+      let ortherTo = this.state.cards.filter(item => {return item.group !== result.destination.droppableId;});
+
+      this.setState({cards: [...toOrder, ...ortherTo]});
+    }
   }
 
   render() {
-    let cards = this.props.cards.sort((a, b) => {
-      if (a.group < b.group)
-        return -1
-      if (a.group > b.group)
-        return 1
-      return 0
-    });
-
     let listCards = [];
-    let tmp = [];
-    let lastGroup = null;
-    cards.forEach((card) => {
-      if (lastGroup !== card.group) {
-        if (tmp.length) {
-          listCards.push(
-            <CardsList cards={tmp} name={lastGroup} key={lastGroup} onDnD={this.handleDnD} />
-          );
-        }
-        tmp = [];
-      }
 
-      lastGroup = card.group;
-
-      tmp.push(card);
-    });
-
-    if (lastGroup) {
+    this.state.groups.forEach(groupName => {
+      let tmp = this.state.cards.filter(item => {return item.group === groupName});
       listCards.push(
-        <CardsList cards={tmp} name={lastGroup} key={lastGroup} onDnD={this.handleDnD} />
+        <CardsList cards={tmp} name={groupName} key={groupName} />
       );
-    }
+    });
 
     return (
-      <div className="board-cards">
-        {listCards}
-      </div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="board-cards">
+          {listCards}
+        </div>
+      </DragDropContext>
     );
   }
 }
 
-
+const GROUPS = [
+  'From',
+  'To'
+]
 
 const CARDS = [
   {id: 'banana', name: 'Banana', group: 'From'},
-  {id: 'apple', name: 'Apple', group: 'To'},
   {id: 'mango', name: 'Mango', group: 'From'},
-  {id: 'orange', name: 'Orange', group: 'From'}
+  {id: 'orange', name: 'Orange', group: 'From'},
+  {id: 'apple', name: 'Apple', group: 'To'}
 ]
 
 ReactDOM.render(
-  <App cards={CARDS} />,
+  <App cards={CARDS} groups={GROUPS} />,
   document.getElementById('root')
 )
